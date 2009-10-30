3bf56f3c79c9d1988eb6bd29bb6c124a5e9d7d92
Andy Gimblett <a.m.gimblett@swansea.ac.uk>
2009-10-30 19:45:57 +0000

    Made Synchronous.hs threadsafe by replacing IORefs with MVars

--- Control/Observer/Synchronous.hs
+++ Control/Observer/Synchronous.hs
@@ -4,8 +4,8 @@
      <http://www.cs.uu.nl/wiki/bin/view/Afp0607/ExerciseWXHaskell>
 
 The 'Control.Observer.Subject' implementation defined in this module
-uses 'IORef's to provide a simple synchronous implementation of the
-Observer design pattern.
+uses 'MVar's to provide a simple and threadsafe synchronous
+implementation of the Observer design pattern.
 
 Note that no constructor for 'Sub' is exported: client code must use
 the 'createSub' smart constructor.
@@ -20,28 +20,35 @@ module Control.Observer.Synchronous (
     createSub
 ) where
 
-import Data.IORef
+import Control.Concurrent.MVar
 
 import Control.Observer
 
--- | Synchronous Subject implementation.
+-- | Threadsafe synchronous Subject implementation.
 
 -- A Sub contains:
---   1) a pointer to the current value;
---   2) a pointer to a list of "notify" functions (the observers).
+--   1) an MVar pointer to the current value;
+--   2) an MVar pointer to a list of "notify" functions (the
+--   observers).
 data Sub a = SubC {
-    value     :: IORef a,
-    observers :: IORef [a -> IO ()]
+    value     :: MVar a,
+    observers :: MVar [a -> IO ()]
    }
 
 instance Subject (Sub a) a where 
-    getValue = readIORef . value
-    setValue'= writeIORef . value
-    addObserver observable = modifyIORef (observers observable) . (:)
-    getObservers = readIORef . observers
+    -- Read current value; would block if it was ever empty, but it
+    -- shouldn't ever be.
+    getValue = readMVar . value
+    -- Write current value; we can't just use putMVar because it
+    -- blocks if the MVar is full.  We want to always overwrite.
+    setValue' sub = modifyMVar_ (value sub) . const . return
+    -- Append an observer to the list of observers.
+    addObserver sub = modifyMVar_ (observers sub) . \x -> return . (++ [x])
+    -- Get the current list of observers.
+    getObservers = readMVar . observers
 
 -- | Smart constructor for Sub.
 createSub :: a -> IO (Sub a)
-createSub val = do value'   <- newIORef val
-                   observers' <- newIORef []
+createSub val = do value'   <- newMVar val
+                   observers' <- newMVar []
                    return $ SubC value' observers'
